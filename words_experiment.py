b29ea4f72a6f74de71dbbe221c621f1d97ad8781
Joris <jsbaan@gmail.com>
2017-12-16 18:58:56 +0100

    Final status of project

---
+++ words_experiment.py
@@ -0,0 +1,36 @@
+# NL-EN Cross-Model Embeddings
+nl_en_same_nl = ['toespraak', 'urine', 'maar', 'missie', 'plant', 'boot', 'sport', 'lamp', 'wereld', 'water']
+nl_en_same_en = ['speech', 'urine', 'but', 'mission', 'plant', 'boat', 'sport', 'lamp', 'world', 'water']
+
+nl_en_diff_nl = ['vis', 'uur', 'het', 'wetenschap', 'mensen', 'primaat', 'richting', 'slaap', 'kleingeld', 'inbox']
+nl_en_diff_en = ['bee', 'blind', 'day', 'homeless', 'street', 'fish', 'hour', 'it', 'science', 'people']
+
+nl_en_similar_nl = ['hond','auto','kinderen','neef','arts','laptop','toerist','muziek','wiskundige','spijkerbroek']
+nl_en_similar_en = ['cat','bus','grandchildren','niece','surgeon','computer','toerism','jazz','physicist','sweater']
+
+# NL-NL Autoencoder Embeddings
+nl_nl_diff_1 = ['soortgelijks', 'spotgoedkoop', 'nazaten', 'marktrevolutie', 'variatie', 'fouten', 'röntgenfoto', 'experts', 'schaalvoordeel', 'verbazingwekkend']
+nl_nl_diff_2 = ['diamanten', 'epicentrum', 'weergaven', 'Keulse', 'wervelkolom', 'katrol', 'gekroond', 'Heiligheid', 'Schotse', 'Waarvan']
+
+nl_nl_same_1 = ['nodig', 'gelijk', 'afval', 'aanhouden', 'subject', 'designer', 'nerveus', 'sekse', 'steen', 'overgeven']
+nl_nl_same_2 = ['noodzakelijk', 'hetzelfde', 'vuil', 'blijven', 'onderwerp', 'ontwerper', 'gespannen', 'geslacht', 'rots', 'braken']
+
+nl_nl_similar_1 = ['hond','auto','kinderen','neef','arts','laptop','toerist','muziek','wiskundige','spijkerbroek']
+nl_nl_similar_2 = ['kat','bus','kleinkinderen','nicht','chirurg','computer','toerisme','jazz','natuurkundige','trui']
+
+# EN-EN Translator Embeddings
+
+en_en_diff_1 = ['fulfilled', 'schmuck', 'scrutinized', 'Foresight', 'Circle', 'interpret', 'multitask', '30', 'conspiring', 'Amerasians']
+en_en_diff_2 = ['Geraldine', 'propolis', 'geographies', 'race', 'bluff', 'Tighter', 'most', 'one-life', 'Westerner', 'X-Files']
+
+en_en_same_1 = ['cubicles', 'visitors', 'immediate', 'rope', 'shrub', 'outcome', 'rest', 'compute', 'draw']
+en_en_same_2 = ['offices', 'guests', 'instantaneous', 'cord', 'bush', 'result', 'relax', 'calculate', 'sketch']
+
+en_en_similar_1 = ['cat','bus','grandchildren','niece','surgeon','computer','toerism','jazz','physicist','sweater']
+en_en_similar_2 = ['dog','car','children','cousin','physician','laptop','tourist','music','mathematician','jeans']
+
+# nl_en_diff_nl = ['rijk', 'bekend', 'gek', 'zwemmers', 'aanstoot', 'afwas', 'charme', 'geleidend', 'pletten', 'pleinen']
+# nl_en_diff_en = ['child', 'protect', 'environment', 'greatest', 'tree', 'risk', 'food', 'answer', 'weapon', 'graph']
+
+# nl_nl_diff_1 = ['neerbuigend', 'implementatie', 'omdoen', 'omarming', 'rotsblok', 'sojabonen', 'gestelde', 'klep', 'gillend', 'deodorant']
+# nl_nl_diff_2 = ['zeggen', 'vandaag', 'kennis', 'media', 'passen', 'context', 'artikel', 'bloed', 'haar', 'balans']
